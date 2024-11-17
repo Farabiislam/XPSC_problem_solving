1a35c8a7cda265c571c6de3b44a3c17571828971
farabi islam <farabii099@gmail.com>
2024-11-17 23:12:58 +0600

    day-1 added

---
+++ week-4/day-1/RCB_vs_CSK.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     ios::sync_with_stdio(false);
+     cin.tie(nullptr);
+    
+    int x,y;
+    cin >> x >> y;
+
+       (x-y >= 18) ? cout << "RCB" : cout << "CSK";
+    
+    return 0;
+}
